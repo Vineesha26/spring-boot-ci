c39058b218db4f5dfce56bc4d13ba81d0dc0022f
Vineesha Reddy <vineeshareddy@Vineeshas-MacBook-Air.local>
2026-02-28 18:56:07 -0600

    update

---
+++ src/main/java/com/project/CloudB/CloudBApplication.java
@@ -0,0 +1,13 @@
+package com.project.CloudB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/CloudB/CloudBController.java
@@ -0,0 +1,14 @@
+package com.project.CloudB;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CloudBController {
+
+    @GetMapping("/")
+    public String cloudB()
+    {
+        return "Hello";
+    }
+}
